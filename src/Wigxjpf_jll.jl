4c7eaaccf7e729e83d7fdeb76644afd312bea86e
jlbuild <juliabuildbot@gmail.com>
2021-10-09 04:33:23 +0000

    Wigxjpf_jll build 1.11.0+0

---
+++ src/Wigxjpf_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Wigxjpf_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Wigxjpf")
+JLLWrappers.@generate_main_file("Wigxjpf", UUID("236017c9-faef-5401-ab52-d2664edfb5d0"))
+end  # module Wigxjpf_jll

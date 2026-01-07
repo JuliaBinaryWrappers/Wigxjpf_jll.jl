0467641c4b757c85e2cb2fcf05559b6e4c48a599
jlbuild <juliabuildbot@gmail.com>
2026-01-07 15:56:23 +0000

    Wigxjpf_jll build 1.13.0+0

--- src/Wigxjpf_jll.jl
+++ src/Wigxjpf_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Wigxjpf")
-JLLWrappers.@generate_main_file("Wigxjpf", UUID("236017c9-faef-5401-ab52-d2664edfb5d0"))
+JLLWrappers.@generate_main_file("Wigxjpf", Base.UUID("236017c9-faef-5401-ab52-d2664edfb5d0"))
 end  # module Wigxjpf_jll
